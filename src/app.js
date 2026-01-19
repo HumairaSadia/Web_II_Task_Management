6681066a7b072b9d2643f6221e177aa3e021f7f3
HumairaSadia <humairasadia11@gmail.com>
2026-01-19 17:42:19 +0600

    CORS is installed in this Backend folder for the different origin or port number

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+
 const express = require('express');
+const cors = require('cors'); // 1. Import the cors package
 const taskRoutes = require('./routes/taskRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 const AppError = require('./utils/AppError');
 
 const app = express();
+
+// 2. Enable CORS so your React app (port 5173) can access this API (port 3000)
+app.use(cors()); 
 
 app.use(express.json());
 
@@ -11,10 +16,11 @@ app.use(express.json());
 app.use('/api/v1/tasks', taskRoutes);
 
 // Handle 404 for undefined routes
-app.all('*all', (req, res, next) => {
+app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Global Error Handler
 app.use(errorHandler);
+
 module.exports = app;
